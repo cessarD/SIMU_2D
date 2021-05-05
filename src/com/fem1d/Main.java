525060cb7df60bb52e3daf103bb68f17627899b3
cessarD <00103518@uca.edu.sv>
2021-05-04 21:26:32 -0600

    creacion de localk y localb

--- src/com/fem1d/Main.java
+++ src/com/fem1d/Main.java
@@ -1,20 +1,40 @@
 package com.fem1d;
 
+import com.fem1d.clases.condition;
 import com.fem1d.clases.element;
 import com.fem1d.clases.mesh;
 import com.fem1d.clases.node;
-
+import com.fem1d.SEL;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        //instanciar archivos externos
+        SEL tools= new SEL();
+
+
         mesh m = new mesh();
+        Vector localb=new Vector();
+        Vector localk=new Vector();
+        //revisar tamaño de matriz final
+        double [][] k = new double[2][2];
+        Vector b=new Vector();
+        Vector T=new Vector();
 	    System.out.println("hi");
         ReadMeshandConditions(m);
+        tools.crearSistemasLocales(m,localk,localb);
+
+
+        //no se como revisar que el localK este cargado
+        // System.out.println(localk.elementAt(0)[0][0]);
+
+
+
+        System.out.println(localb.get(2));
     }
 
 
@@ -31,8 +51,10 @@ public class Main {
 
 
             //entrydata= new File("/home/rene/Documents/Ciclo2021/codigosidequest/SIMU/src/com/fem1d/clases/problem.msh");
+            //System.out.println(entrydata.exists());
+
                     entrydata= new File("C:/Users/cesar/IdeaProjects/SIMU2/src/com/fem1d/clases/problem.msh");
-            System.out.println(entrydata.exists());
+
         }while(!entrydata.exists());
 
         //archivo abierto, buscando variables de archivo msh
@@ -57,31 +79,45 @@ public class Main {
                 m.setSizes(nnodes,neltos,ndirich,nneuman);
                 //crear data
                 //m.createData();
+            for(int i=0;i<3;i++){
+                reader.nextLine();
+            }
 
-                reader.nextLine();
-                reader.nextLine();
-                reader.nextLine();
+
             for (int i=0;i<nnodes;i++){
                 node n = new node();
                 n.setnode(reader.nextInt(), reader.nextFloat());
                 m.getNodes().add(i,n);
 
             }
-            reader.nextLine();
-            reader.nextLine();
-            reader.nextLine();
-            reader.nextLine();
+            for(int i=0;i<4;i++){
+                reader.nextLine();
+            }
             for (int i=0;i<neltos;i++){
                 element n = new element();
                 n.setelement(reader.nextInt(), reader.nextInt(), reader.nextInt());
                 m.getElements().add(i,n);
 
             }
-            System.out.println("elementos");
-            for (int i=0;i<neltos;i++){
-
-                System.out.println(m.getElements().get(i).getNode1());
+            for(int i=0;i<4;i++){
+                reader.nextLine();
             }
+            for (int i=0;i<ndirich;i++){
+                condition n = new condition();
+                n.setcondition(reader.nextInt(), reader.nextFloat());
+                m.getDirichlet().add(i,n);
+
+            }
+            for(int i=0;i<4;i++){
+                reader.nextLine();
+            }
+            for (int i=0;i<nneuman;i++){
+                condition n = new condition();
+                n.setcondition(reader.nextInt(), reader.nextFloat());
+                m.getNeumann().add(i,n);
+
+            }
+
 
 
 
---
+++ src/com/fem1d/SEL.java
@@ -0,0 +1,75 @@
+package com.fem1d;
+
+import com.fem1d.clases.mesh;
+import java.util.*;
+public class SEL {
+    enum lines {NOLINE,SINGLELINE,DOUBLELINE};
+    enum modes {NOMODE,INT_FLOAT,INT_INT_INT};
+    enum parameter {ELEMENT_LENGTH,THERMAL_CONDUCTIVITY,HEAT_SOURCE};
+    enum size {NODES,ELEMENTS,DIRICHLET,NEUMANN};
+
+
+    double[][] createLocalK(int element,mesh m){
+        //Se prepara la matriz y sus dos filas (se sabe que es una matriz 2x2)
+        double [][] matriz = new double[2][2];
+        Vector row1=new Vector(), row2=new Vector();
+
+        //De acuerdo a la formulación, la matriz local K tiene la forma:
+        //          (k/l)*[ 1 -1 ; -1 1 ]
+
+        //Se extraen del objeto mesh los valores de k y l
+        float k = m.getParameter(parameter.THERMAL_CONDUCTIVITY.ordinal()), l = m.getParameter(parameter.ELEMENT_LENGTH.ordinal());
+        //Se crean las filas
+        row1.add(0,k/l);row1.add(0,-k/l);
+        row2.add(0,-k/l);row2.add(0,k/l);
+
+        //Se insertan las filas en la matriz
+
+
+
+        //POSIBLEMENTE ME MAME AQUI
+        matriz[0][0]=new Double(row1.get(0).toString()); matriz[0][1]=new Double(row1.get(1).toString());
+        matriz[1][0]=new Double(row2.get(0).toString()); matriz[1][1]=new Double(row2.get(1).toString());
+
+
+
+
+       // for(int i=0;i<2;i++){
+            //for(int j=0;j<2;j++){
+               // System.out.println(matriz[i][j]);
+          //  }
+     //   }
+      //  System.out.println("cambio de local");
+        return matriz;
+    }
+
+    Vector createLocalb(int element,mesh m){
+        //Se prepara el vector b (se sabe que será un vector 2x1)
+        Vector b=new Vector();;
+
+        //Se sabe que el vector local b tendrá la forma:
+        //          (Q*l/2)*[ 1 ; 1 ]
+
+        //Se extraen del objeto mesh los valores de Q y l
+        float Q = m.getParameter(parameter.HEAT_SOURCE.ordinal()), l = m.getParameter(parameter.ELEMENT_LENGTH.ordinal());
+        //Se insertan los datos en el vector
+        b.add(Q*l/2); b.add(Q*l/2);
+
+        return b;
+    }
+
+
+
+    void crearSistemasLocales(mesh m, Vector localKs, Vector localbs){
+        //Se recorren los elementos
+        System.out.println("archivo"+m.getSize(size.ELEMENTS.ordinal()));
+        for(int i=0;i<m.getSize(size.ELEMENTS.ordinal());i++){
+            //Por cada elemento, se crea su K y su b
+            localKs.addElement(createLocalK(i,m));
+           localbs.addElement(createLocalb(i,m));
+
+        }
+
+    }
+
+}
